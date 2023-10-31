e8b151a62ebc64929ecc7b84742faff86da2618c
neerajkr-coding <134834249+neerajkr-coding@users.noreply.github.com>
2023-10-31 20:57:03 +0530

    Add files via upload

---
+++ prac1.java
@@ -0,0 +1,27 @@
+
+	
+	public static void findOccurance(String main,String str) {
+		
+		int count=0;
+		
+		for(int i =0;i<main.length();i++) {
+			char ch = main.charAt(i);
+			
+			
+			if(ch==str.charAt(0)) {
+				int temp = i;
+				for(int j=0;(temp+j)<main.length()&&j<str.length();j++) {
+					if(main.charAt(temp+j)!=str.charAt(j)) {
+						break;
+					}
+					if(str.length()-1==(j)) {
+						count++;
+					}
+				}
+			}
+			
+		}
+		System.out.println(count);	
+	}
+
+	
